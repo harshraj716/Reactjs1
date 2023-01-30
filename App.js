34ff0ec77a613c5a425145286bbb502f60a1ed38
Harsh Raj <harshraj07167@gmail.com>
2023-01-30 11:11:40 +0530

    "react"

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Heading from './component/Heading';
+import Section1 from './component/Section1';
+import Section2 from './component/Section2';
+function App() {
+  return (
+    <div className="App">
+      <Heading/>
+      <Section1/>
+      <Section2/>
+    </div>
+  );
+}
+
+export default App;
